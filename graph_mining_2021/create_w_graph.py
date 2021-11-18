d97de06db96dc1df01e6861706f7840b9762bbb8
Serena Sian Yuan <ssy248@nyu.edu>
2021-11-17 17:24:31 -0800

    Create create_w_graph.py

---
+++ graph_mining_2021/create_w_graph.py
@@ -0,0 +1,16 @@
+# create weighted graph
+
+# sample input create_w_graph([[0,1, w_1],[1,2, w_2],[2,3, w_3],[0,3, w_4]])
+
+def create_w_graph(edgelist):
+    n_vertices = np.max(edgelist)+1
+    # create adjacency matrix
+    adj_mat = np.zeros((n_vertices, n_vertices))
+    
+    for e1, e2, w in edgelist:
+        adj_mat[e1][e2] = w
+        adj_mat[e2][e1] = w
+
+    
+    return adj_mat
+    
