197bcfd12473542b8d491c49b919c74200cebcf6
Serena Sian Yuan <ssy248@nyu.edu>
2021-11-29 17:04:04 -0500

    Create triangle_quotient.py

---
+++ graph_mining_2021/triangle_quotient.py
@@ -0,0 +1,17 @@
+import networkx as nx
+g = nx.Graph()
+
+g.add_nodes_from([0,1,2, 3,4,5])
+
+g.add_edges_from([(1, 2), (1, 3), (2,3), (4,5), (1,4)])
+
+# quotient graph , by triangles
+
+# https://networkx.org/documentation/stable/tutorial.html
+
+# https://www.kite.com/python/docs/networkx.quotient_graph
+
+all_cliques= nx.enumerate_all_cliques(g)
+
+triad_cliques=[x for x in all_cliques if len(x)==3 ]
+
