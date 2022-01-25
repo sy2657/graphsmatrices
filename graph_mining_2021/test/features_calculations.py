27fc41a74eddd7d9a5e589722ea783ed90fb3797
Serena Sian Yuan <ssy248@nyu.edu>
2022-01-24 16:34:49 -0800

    Create features_calculations.py

---
+++ graph_mining_2021/test/features_calculations.py
@@ -0,0 +1,23 @@
+# features: max clique size
+# touching number
+# max degree
+# degree distn 
+max_clique_sizes = []
+
+touching_numbers = []
+
+
+# clique_sizes = []
+
+
+all_cliques = nx.enumerate_all_cliques(g)
+
+clique_sizes = [len(x) for x in all_cliques]
+
+max_clique_size = max(clique_sizes)
+
+deg_dist = []
+
+for row in adj_mat:
+    rsum = sum(row)
+    deg_dist.append(rsum)
